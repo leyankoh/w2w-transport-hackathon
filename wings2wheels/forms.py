090d0e6bea8f4b4d7788b5dd92c75b5c1fe271d7
Le Yan Koh <leyankoh@gmail.com>
2019-02-23 20:55:46 +0000

    update forms

--- wings2wheels/forms.py
+++ wings2wheels/forms.py
@@ -1,35 +1,23 @@
 from django import forms
-import datetime
+from .models import UserForm
+from django.forms import ModelForm
 
-class UserForm(forms.Form):
-    """
-    booking form to gather information about the passenger
-    we will need to know:
-    name
-    gender
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
-    phone number
-    email
-    flight number
-    dates
-    time
-
-    PREFERENCES
-    mode of transport
-    single gender?
-    allergies
-    chatty?
-    """
-    GENDER_CHOICES = [('female', 'Female'),
-                      ('male', 'Male')]
-
-    name = forms.CharField(max_length=200)
-    gender = forms.CharField(widget=forms.Select(choices=GENDER_CHOICES))
-    email = forms.EmailField()
-    phone_number = forms.IntegerField()
-    flight_number = forms.CharField(max_length=10)
-    date = forms.DateTimeField()
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
 
 
-class PreferenceForm(forms.Form):
-    
+GENDER_CHOICES = [('female', 'Female'),
+                  ('male', 'Male')]
+
+class User(ModelForm):
+    gender = forms.ChoiceField(widget=forms.RadioSelect, choices=GENDER_CHOICES)
+    class Meta:
+        model = UserForm
+        fields = ['name','gender', 'email', 'phone_number', 'flight_number', 'date', 'time',"mode"]
+        widgets = {
+            'date': DateInput(),
+            'time': TimeInput(),
+        }
